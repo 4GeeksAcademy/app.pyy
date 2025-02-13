a609a406fdadd8aaf8becfba1ec90a4fed070de7
Facudlomas <Facundo.mosquera.cala@gmail.com>
2025-02-13 13:00:49 +0000

    mas pruebas

--- src/app.py
+++ src/app.py
@@ -9,7 +9,7 @@ from pickle import load
 
 #print(os.getcwd())
 
-label_encoder_city_of_birth=load(open("./le_city_of_birth.sav", "rb"))
+#label_encoder_city_of_birth=load(open("./le_city_of_birth.sav", "rb"))
 label_encoder_country_of_birth=load(open("./le_country_of_birth.sav", "rb"))
 label_encoder_competition_id=load(open("./le_competition_id.sav", "rb"))
 label_encoder_club_name=load(open("./le_club_name.sav", "rb"))
@@ -18,22 +18,12 @@ label_encoder_sub_position=load(open("./le_sub_position.sav", "rb"))
 label_encoder_position=load(open("./le_position.sav", "rb"))
 
 def cargar_modelo_comprimido(ruta):
-    print('entro')
     """Carga el modelo comprimido con gzip."""
     if not os.path.exists(ruta):
-        print('not path.exists')
         raise FileNotFoundError(f"El archivo {ruta} no existe. Verifica que está en la carpeta correcta.")
-    
-    print('previo al gzip')
     with gzip.open(ruta, "rb") as f:
-        print('while carga')
         modelo = joblib.load(f)
-        print('while carga fin')	
-    
-    print('salida')
     return modelo
-
-
 
 # Define la ruta del modelo comprimido
 RUTA_MODELO = "./modelo.pkl.gz"
@@ -82,13 +72,13 @@ age = st.sidebar.slider('age', 0, 100, 25)
 height_in_cm = st.sidebar.slider('height_in_cm', 0, 100, 25)
 highest_market_value_in_eur = st.sidebar.slider('highest_market_value_in_eur', 0, 100, 25)
 
-club_name = st.selectbox('Select a club name', ["boca","river"])
-foot = st.selectbox('Select a foot', ["izquierda","derecha"])
-position = st.selectbox('Select a position', ["medio", "defensa"])
-sub_position = st.selectbox('Select a sub position', ["mediocampista", "defensor"])
-country_of_birth = st.selectbox('Select a country of birth', ["argentina", "brasil"])
-city_of_birth= st.selectbox('Select a city of birth', ["lomas", "lanus"])
-competition_id = st.selectbox('Select a competition', ["liga","copa"])
+club_name = st.sidebar.selectbox('Select a club name', ["boca","river"])
+foot = st.sidebar.selectbox('Select a foot', ["izquierda","derecha"])
+position = st.sidebar.selectbox('Select a position', ["medio", "defensa"])
+sub_position = st.sidebar.selectbox('Select a sub position', ["mediocampista", "defensor"])
+country_of_birth = st.sidebar.selectbox('Select a country of birth', ["argentina", "brasil"])
+#city_of_birth= st.selectbox('Select a city of birth', ["lomas", "lanus"])
+competition_id = st.sidebar.selectbox('Select a competition', ["liga","copa"])
 
 
 #club_name = st.selectbox('Select a club name', df_club_name)
@@ -106,10 +96,10 @@ foot_le = label_encoder_foot.transform([foot])[0]
 position_le = label_encoder_position.transform([position])[0]
 sub_position_le = label_encoder_sub_position.transform([sub_position])[0]
 country_of_birth_le = label_encoder_country_of_birth.transform([country_of_birth])[0]
-city_of_birth_le = label_encoder_city_of_birth.transform([city_of_birth])[0]
+#city_of_birth_le = label_encoder_city_of_birth.transform([city_of_birth])[0]
 
 
-info=[matches_played, yellow_cards, red_cards, goals, assists, minutes_played, age, height_in_cm, highest_market_value_in_eur, competition_id_le, club_name_le, foot_le, position_le, sub_position_le, country_of_birth_le,  city_of_birth_le]
+info=[matches_played, yellow_cards, red_cards, goals, assists, minutes_played, age, height_in_cm, highest_market_value_in_eur, competition_id_le, club_name_le, foot_le, position_le, sub_position_le, country_of_birth_le]
 
 
 if st.button("Realizar predicción"):
@@ -119,4 +109,3 @@ if st.button("Realizar predicción"):
         st.write(f"Predicción del modelo: {prediccion}")
     except Exception as e:
         st.error(f"Error al realizar la predicción: {str(e)}")
-
